667159f74d91f02ed34efb93ad8f99b9d36652a6
HangHuynh1998 <huynhhang.24hdev@gmail.com>
2020-12-07 01:24:02 +0700

    add search tour

--- config/routes/api/tour.js
+++ config/routes/api/tour.js
@@ -8,8 +8,13 @@ const {
 router.post("/", jwtAuth, hasRole('company'), tourCtrl.addTour);
 router.get("/", tourCtrl.getAllTour);
 router.get("/toursale", tourCtrl.getTourSale);
+router.get("/place", tourCtrl.getAllTourPlace);
+router.get("/name", tourCtrl.getAllTourName);
+router.get("/company", tourCtrl.getAllTourCompany);
+router.get("/price", tourCtrl.getAllTourPrice);
 router.get("/:id/category", tourCtrl.getCategoryTours);
 router.get("/:id", tourCtrl.getTourDetails);
 router.put("/:id", jwtAuth, hasRole('company'), tourCtrl.editTour);
 router.delete("/:id",jwtAuth, hasRole('company'), tourCtrl.deleteTour);
+
 module.exports = router
--- controllers/api/tours.controller.js
+++ controllers/api/tours.controller.js
@@ -9,6 +9,70 @@ const getAllTour = (req, res) => {
             res.sendError(err.message);
         })
 };
+const getAllTourPlace = (req, res) => {
+    tourService.getAllTours(req.query)
+    .then(data => {
+        console.log("da",req.body);
+        var datasale =[]
+        for(var i in data){
+            if(data[i].place.toLowerCase().indexOf(req.body.searchPlace.toLowerCase()) !== -1){
+                datasale.push(data[i])
+            }
+        }
+        res.sendData(datasale);
+    })
+    .catch(err => {
+        res.sendError(err.message);
+    })
+};
+const getAllTourName = (req, res) => {
+    tourService.getAllTours(req.query)
+    .then(data => {
+        console.log("da",req.body);
+        var dataname =[]
+        for(var i in data){
+            if(data[i].name.toLowerCase().indexOf(req.body.searchName.toLowerCase()) !== -1){
+                dataname.push(data[i])
+            }
+        }
+        res.sendData(dataname);
+    })
+    .catch(err => {
+        res.sendError(err.message);
+    })
+};
+const getAllTourCompany = (req, res) => {
+    tourService.getAllTours(req.query)
+    .then(data => {
+        console.log("da",req.body);
+        var dataname =[]
+        for(var i in data){
+            if(data[i].company_id.user_id.name.toLowerCase().indexOf(req.body.searchCompany.toLowerCase()) !== -1){
+                dataname.push(data[i])
+            }
+        }
+        res.sendData(dataname);
+    })
+    .catch(err => {
+        res.sendError(err.message);
+    })
+};
+const getAllTourPrice = (req, res) => {
+    tourService.getAllTours(req.query)
+    .then(data => {
+        console.log("da",req.body);
+        var dataname =[]
+        for(var i in data){
+            if(data[i].price <= req.body.searchPrice){
+                dataname.push(data[i])
+            }
+        }
+        res.sendData(dataname);
+    })
+    .catch(err => {
+        res.sendError(err.message);
+    })
+};
 const getTourSale= (req, res) => {
     tourService.getAllTours(req.query)
         .then(data => {
@@ -77,6 +141,10 @@ const deleteTour = (req, res) => {
 module.exports = {
     addTour,
     getAllTour,
+    getAllTourPlace,
+    getAllTourName,
+    getAllTourCompany,
+    getAllTourPrice,
     getTourSale,
     getCategoryTours,
     getTourDetails,
